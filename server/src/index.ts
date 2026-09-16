1a9d1175e1a0976300703c254ec7882c7164aa76
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2026-09-15 22:49:35 -0300

    feat: send message

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,9 @@ import "dotenv/config";
 import express from "express";
 import { createServer } from "node:http";
 import { join } from "node:path";
+import { Server } from "socket.io";
 import { config } from "./config";
+import { SocketEvents } from "./socket/events";
 
 async function main() {
 	const app = express();
@@ -11,9 +13,16 @@ async function main() {
 	app.get("/health", (_req, res) => res.json({ ok: true }));
 
 	const httpServer = createServer(app);
+	const io = new Server(httpServer);
 
 	app.get("/", (req, res) => {
 		res.sendFile(join(__dirname, "index.html"));
+	});
+
+	io.on("connection", (socket) => {
+		socket.on(SocketEvents.UserJoined, (username) => {
+			console.log("teste", username);
+		});
 	});
 
 	httpServer.listen(config.port, "0.0.0.0", () => {
---
+++ server/src/socket/events.ts
@@ -0,0 +1,3 @@
+export const SocketEvents = {
+	UserJoined: "user:joined",
+} as const;
